94470876ac58b4653078c1a5853da16dc22e8ade
Marlysson <marlysson5@gmail.com>
2020-12-26 03:22:30 -0300

    Separated some data files to data folder.

---
+++ src/data/colors.js
@@ -0,0 +1,9 @@
+const COLOR_BY_RANKING = {
+    "libertadores" : "#3498db",
+    "pre_libertadores" : "#2ecc71",
+    "sul_americana" : "#f1c40f",
+    "rebaixamento" : "#e74c3c",
+    "nenhum" : "#bdc3c7"
+}
+
+export default COLORS_BY_RANKING
---
+++ src/data/teams.js
@@ -0,0 +1,105 @@
+const TEAMS = [
+    { 
+        "name": "Athletico-PR",
+        "initials": "CAP",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/09/09/Athletico-PR.svg",
+    },
+    {
+        "name": "Atlético-GO",
+        "initials": "ACG",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2020/07/02/atletico-go-2020.svg"
+    },
+    {
+        "name": "Atlético-MG",
+        "initials": "CAM",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/10/atletico-mg.svg"
+    },
+    {
+        "name": "Bahia",
+        "initials": "BAH",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/bahia.svg"
+    },
+    {
+        "name": "Botafogo",
+        "initials": "BOT",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/02/04/botafogo-svg.svg"
+    },
+    {
+        "name": "Bragantino",
+        "initials": "BGT",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/12/30/svg.svg"
+    },
+    {
+        "name": "Ceará",
+        "initials": "CEA",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/10/10/ceara.svg"
+    },
+    {
+        "name": "Corinthians",
+        "initials": "COR",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/09/30/Corinthians.svg"
+    },
+    {
+        "name": "Coritiba",
+        "initials": "CFC",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/coritiba.svg"
+    },
+    {
+        "name": "Flamengo",
+        "initials": "FLA",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/04/10/Flamengo-2018.svg"
+    },
+    {
+        "name": "Fluminense",
+        "initials": "FLU",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/fluminense.svg"
+    },
+    {
+        "name": "Fortaleza",
+        "initials": "FOR",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/06/10/optimised.svg"
+    },
+    {
+        "name": "Goiás",
+        "initials": "GOI",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/05/01/Goias_SVG.svg"
+    },
+    {
+        "name": "Grêmio",
+        "initials": "GRE",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/12/gremio.svg"
+    },
+    {
+        "name": "Internacional",
+        "initials": "INT",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/internacional.svg"
+    },
+    {
+        "name": "Palmeiras",
+        "initials": "PAL",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2019/07/06/Palmeiras.svg"
+    },
+    {
+        "name": "Santos",
+        "initials": "SAN",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/12/santos.svg"
+    },
+    {
+        "name": "São Paulo",
+        "initials": "SAO",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/sao-paulo.svg"
+    },
+    {
+        "name": "Sport",
+        "initials": "SPT",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/sport.svg"
+    },
+    {
+        "name": "Vasco",
+        "initials": "VAS",
+        "logo": "https://s.glbimg.com/es/sde/f/organizacoes/2018/03/11/vasco.svg"
+    }
+    
+]
+
+export default TEAMS
