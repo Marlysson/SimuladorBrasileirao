0e709549d57fe9ed6ffe8106d9f22b1f9365cd98
Marlysson <marlysson5@gmail.com>
2022-12-31 11:32:58 -0300

    New folder's structure.

--- src/data/colors.js
+++
@@ -1,9 +0,0 @@
-const COLOR_BY_RANKING = {
-    "libertadores" : "#3498db",
-    "pre_libertadores" : "#2ecc71",
-    "sul_americana" : "#f1c40f",
-    "rebaixamento" : "#e74c3c",
-    "nenhum" : "#bdc3c7"
-}
-
-export default COLOR_BY_RANKING
--- src/data/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    "TOKEN" : process.env.VUE_APP_TOKEN,
-    "FOOTBALL_API": "https://api.football-data.org/v2/"
-};
