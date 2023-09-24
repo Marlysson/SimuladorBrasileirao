f712692e18f78a4016dfd30871405f403ad37f5a
Marlysson <marlysson5@gmail.com>
2023-09-24 12:47:54 -0300

    Refactored resources to simplify calls to api.

---
+++ back/core/integration/resources.py
@@ -0,0 +1,34 @@
+import requests
+
+
+class Resource:
+    headers = {"X-Auth-Token": '7cb43051be1a429b997906d370d5f0bb'}
+    base = "http://api.football-data.org/v4"
+    
+    @classmethod
+    def request(cls, resource, **parameters):
+        endpoint = resource.format(base=cls.base, **parameters)
+        return requests.get(endpoint, headers=cls.headers)
+      
+
+class Standing(Resource):
+    competition = 'BSA'
+    resource = "{base}/competitions/{competition}/standings"
+    
+    @classmethod
+    def get(cls):
+        parameters = {"competition": cls.competition}
+        response = cls.request(cls.resource, **parameters)        
+        return response.json()['standings'][0]['table']
+
+
+class Team(Resource):
+    competition_id = 2013
+    resource = "{base}/competitions/{competition_id}/teams"
+
+    @classmethod
+    def get(cls):
+        parameters = {"competition_id": cls.competition_id}
+        response = cls.request(cls.resource, **parameters)
+        return response.json()["teams"]
+    
