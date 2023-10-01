a905354d02418c9ff422f5c89aef15e28f9f0541
Marlysson <marlysson5@gmail.com>
2023-10-01 11:32:59 -0300

    Added start and last date to each round and some model definition to allow null values to future matches.

--- back/core/models.py
+++ back/core/models.py
@@ -12,6 +12,8 @@ class Round(models.Model):
     number = models.IntegerField() 
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
     closed = models.BooleanField(default=False) # Avoid updated after round is closed
+    first_match = models.DateTimeField(null=True)
+    last_match = models.DateTimeField(null=True)
 
     class Meta:
         db_table = "round"
@@ -19,15 +21,15 @@ class Round(models.Model):
 
 class Match(models.Model):
 
-    SCHEDULED = "Scheduled"
-    PLAYING = "Playing"
-    FINISHED = "Finished"
-    
+    SCHEDULED = "SCHEDULED"
+    PLAYING = "PLAYING"
+    FINISHED = "FINISHED"
+
     home = models.ForeignKey('Team', on_delete=models.CASCADE, related_name="home_matches")
     away = models.ForeignKey('Team', on_delete=models.CASCADE, related_name="away_matches")
     round = models.ForeignKey(Round, on_delete=models.CASCADE, related_name="matches")
-    goals_home = models.IntegerField(default=0)
-    goals_away = models.IntegerField(default=0)
+    goals_home = models.IntegerField(null=True)
+    goals_away = models.IntegerField(null=True)
 
     date = models.DateTimeField()
 
