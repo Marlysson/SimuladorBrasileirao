c57e09d42a41a57eeadf108c67062d0d19be2960
Marlysson <marlysson5@gmail.com>
2023-10-01 12:16:13 -0300

    Refactored the way of resources are retrieved from api.

--- back/core/integration/resources.py
+++ back/core/integration/resources.py
@@ -3,17 +3,27 @@ import requests
 
 class Resource:
     headers = {"X-Auth-Token": '7cb43051be1a429b997906d370d5f0bb'}
-    base = "http://api.football-data.org/v4"
-    
+    base = "http://api.football-data.org/v4/competitions/{competition}/{resource}"
+    competition_id = 2013 # BRASILEIRÃO SÉRIE A
+
     @classmethod
     def request(cls, resource, **parameters):
-        endpoint = resource.format(base=cls.base, **parameters)
+
+        parameters.update(
+            {
+                "competition": cls.competition_id,
+                "resource": resource
+            }
+        )
+        
+        endpoint = cls.base.format(**parameters)
+        endpoint = endpoint.format(**parameters)
+        
         return requests.get(endpoint, headers=cls.headers)
       
 
-class Standing(Resource):
-    competition = 'BSA'
-    resource = "{base}/competitions/{competition}/standings"
+class Standing(Resource):    
+    resource = "standings"
     
     @classmethod
     def get(cls):
@@ -22,19 +32,30 @@ class Standing(Resource):
         return response.json()['standings'][0]['table']
 
 
-class TeamResource(Resource):
-    competition_id = 2013
-    resource = "{base}/competitions/{competition_id}/teams?season={year}"
+class TeamResource(Resource):    
+    resource = "teams?season={year}"
 
     @classmethod
     def get(cls):
         from datetime import date
-
+        
         parameters = {
-            "competition_id": cls.competition_id, 
             "year": date.today().year
         }
 
         response = cls.request(cls.resource, **parameters)
         return response.json()["teams"]
     
+
+class MatchesResource(Resource):
+    resource = "matches?season={season}"
+
+    @classmethod
+    def get(cls):
+        from datetime import date
+        parameters = {
+            "season": date.today().year
+        }
+
+        response = cls.request(cls.resource, **parameters)
+        return response.json()["matches"]
